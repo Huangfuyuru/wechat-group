110c8c057dd347e8ff27f3d7ad3032cb1a303b21
Huangfuyuru <https://github.com/Huangfuyuru/JS-advanced-lessons.git>
2019-11-28 09:54:46 +0800

    a

--- back-end/routes/childGrow.js
+++ back-end/routes/childGrow.js
@@ -4,8 +4,8 @@ const router = express.Router();
 router.get('/',function(req,res){
     res.end('/')
 })
-router.get('/login',function(req,res){
-    res.end('login')
+router.get('/add',function(req,res){
+    res.end('add')
 })
 
 module.exports = router
