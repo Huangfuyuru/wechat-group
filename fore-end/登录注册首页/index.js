0f5c2d62b7998ef76ba637cb70a68e327aae597d
wuxueyao <2693088968@qq.com>
2019-11-28 19:20:32 +0800

    路由

---
+++ fore-end/登录注册首页/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd-mobile/dist/antd-mobile.css'
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Start from './container/Start';
+import Menus from './container/Menus';
+import Index from './container/Index';
+
+ReactDOM.render(
+    <Index />, 
+    document.getElementById('root')
+);
+
+serviceWorker.unregister();
