8448c8ad1275638d125e7deb3a107ad8a4e94aea
yingzi <yingzi@example.com>
2019-12-03 16:42:11 +0800

    12-3

--- back-end/app-lmy.js
+++ back-end/app-lmy.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 // 引入模块
 const login = require('./routes/login');
+const childGrow = require('./routes/childGrow');
+
+
 const app = new express();
 
 app.use('/menus/login',login);
+app.use('/child/cgrowup',childGrow);
 
 app.listen(3000);
 
