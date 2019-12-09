994be5dd611b486ee4af8d27ea13a64e378d240a
yangxindi2017 <694076236@qq.com>
2019-12-09 10:16:15 +0800

    13.9

--- back-end/routes/lover/loveSound.js
+++ back-end/routes/lover/loveSound.js
@@ -1,9 +1,19 @@
 const express = require('express'),
       app = express(),
-      router = express.Router();
+      router = express.Router(),
+      qs =require('querystring'),
+      bodyParser = require("body-parser"),
+      user =require('../database/dateMethod'),
+      info={} //后端返回给前端的信息
 
 router.get('/',function(req,res,next){
     res.end('hello loveSound');
+})
+
+
+router.get('/lsound',async function(req,res,next){
+    lid = req.query.id;
+    console.log(lid);
 })
 
 module.exports = router;
--- back-end/routes/register.js
+++ back-end/routes/register.js
@@ -1,13 +1,10 @@
 //注册后台
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
 var qs =require('querystring');
 var bodyParser = require("body-parser");
 var user =require('../database/dateMethod');
-var info={} //后端返回给前端的信息
-
-
+var info={code:1,msg:"请输入验证码"} //后端返回给前端的信息
 
 //此中间件的作用是获得请求体字符串，然后转成对象赋值给req.body
 router.use(bodyParser.urlencoded({extended:true}));
@@ -22,7 +19,7 @@ router.post('/confirm',async function(req,res,next){
 
     //可以注册返回0，不存在数据库中   
     var eTel = await user.userM.findTel(req.body.utel);
-    console.log(eTel);
+    console.log('eTel',eTel);
  
     //判断用户电话是否在DB中，若在不让注册，不在可以
     if(eTel === 0){ 
@@ -42,31 +39,45 @@ router.post('/confirm',async function(req,res,next){
 
 });
 
+
 router.post('/', async function(req,res,next){
   
-    console.log('resign body',req.body);
+    // console.log('resign body',req.body);
+    if(info.code === 1){
+        res.json(info);
+    }else{
+        // console.log('code =0了');
+        // console.log(info);
+        // console.log('body2',req.body);
 
-    if(req.body.confirm !== '' && req.body.passwd===req.body.pass){
-        info={
-            code:0,
-            msg:'注册成功！'
-        }
-        res.json(info);
-    }else if(req.body.confirm ===''){
-        info={
-            code:1,
-            msg:'验证码错误'
-        }
-        res.json(info);
+        if(req.body.confirm !== '' && req.body.passwd===req.body.pass){
+            info={
+                code:0,
+                msg:'注册成功！'
+            }
+            // 插入数据
+            var person ={
+                pass:req.body.passwd,
+                tel:req.body.utel
+            }
+            await user.userM.addUser(person);
+            console.log(await user.userM.findAll());
+            res.json(info);
+        }else if(req.body.confirm ===''){
+            info={
+                code:1,
+                msg:'验证码错误'
+            }
+            res.json(info);
 
-    }else if(req.body.pass !== req.body.passwd){
-        info={
-            code:2,
-            msg:'两次密码不一致'
+        }else if(req.body.pass !== req.body.passwd){
+            info={
+                code:2,
+                msg:'两次密码不一致'
+            }
+            res.json(info);
         }
-        res.json(info);
     }
-
 
 });
 
