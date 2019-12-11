0a19a844d007eaafe7d16cecb54c4c027f7d8c3b
1823yangxiaoyun <1762098303@qq.com>
2019-12-11 17:48:26 +0800

    ten

--- fore-end/demo/remember/src/container/lover/Lsouvenir.js
+++ fore-end/demo/remember/src/container/lover/Lsouvenir.js
@@ -44,12 +44,12 @@ export default class Lsouvenir extends Component {
               {/* <img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1996765656,1986455071&fm=26&gp=0.jpg" alt="" style={{height:"40%",width:"92%",margin:"4% 0 0 4%"}}></img> */}
                {
                    this.state.arr.map((item)=>(
-                    <div className="loversou-first">
+                    <div className="loversou-first" style={{position:"static"}}>
                     <img  src="https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=60605163,304913415&fm=26&gp=0.jpg" alt=""  style={{float:"left",height:"100%",width:"50%"}}></img>
                     <h1 style={{marginTop:"8%",textAlign:"center"}}>{item.name}</h1>
                     <p style={{fontSize:"5vw" ,textAlign:"center"}}>{item.date.split("T")[0]}</p>
                      {/* <textarea style={{fontSize:"5vw",float:"right",width:"53%",height:"80%",border:"0.5px solid #888888"}} readonly="readonly">{item.content}</textarea> */}
-                   
+                   <img alt="" src={require("../../image/la.jpg")} style={{float:"right"}}/>
                     </div>
                    ))
                }
