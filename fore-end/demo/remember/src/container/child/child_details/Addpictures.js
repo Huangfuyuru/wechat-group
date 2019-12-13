94c6bfe4845f34504fbf42eb7d72921c6a8e689d
wuxueyao <2693088968@qq.com>
2019-12-13 21:06:35 +0800

    相册

---
+++ fore-end/demo/remember/src/container/child/child_details/Addpictures.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Addpictures extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
