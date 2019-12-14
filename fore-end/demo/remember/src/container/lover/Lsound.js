f56c05682ed56381e9d99ecb72d99a8b942a12a3
yangxindi2017 <694076236@qq.com>
2019-12-14 21:57:08 +0800

    sound

--- fore-end/demo/remember/src/container/lover/Lsound.js
+++ fore-end/demo/remember/src/container/lover/Lsound.js
@@ -6,7 +6,18 @@ export default class Lsound extends Component {
         super(props);
         this.state={
             lover_id:JSON.parse(localStorage.getItem('lid')),
-            arr:[]
+            name:'',
+            voiceurl:'',
+            setdate:'',
+            loverVoiceid:'',
+            arr:[
+                {
+                    name:'语音',
+                    voiceurl:'地址',
+                    setdate:'2019-12-14',
+                    loverVoived:''
+                }
+            ]
         }
     }
     
@@ -16,7 +27,43 @@ export default class Lsound extends Component {
         fetch(`http://localhost:3001/lover/lsound?loverid=${this.state.lover_id}`)
         .then(res=>res.json())
         .then(json=>{
-            console.log('json',json);
+            this.setState((state)=>{
+                if(json.code === 0){
+                    state.arr=json.msg
+                }else{
+                    console.log('json',json)
+                }
+            })
+            console.log('arr',this.state.arr);
+        })
+    }
+
+    componentDidUpdate(){
+        fetch(`http://localhost:3001/lover/lsound?loverid=${this.state.lover_id}`)
+        .then(res=>res.json())
+        .then(json=>{
+            this.setState((state)=>{
+                if(json.code === 0){
+                    state.arr=json.msg
+                }else{
+                    console.log('json',json)
+                }
+            })
+            console.log('arr',this.state.arr);
+        })
+    }
+
+    delSound=(lid,id)=>{
+        console.log('deletesound');
+        fetch(`http://localhost:3001/lover/lsound/lrsound?loverid=${lid}&loverVoiceid=${id}`,{
+            method:'GET'
+        })
+        .then(res=>res.josn)
+        .then(res=>{
+            console.log(res);
+            // this.setState({
+
+            // })
         })
     }
     render() {
@@ -42,7 +89,31 @@ export default class Lsound extends Component {
                     >语音记事</span>
                 </NavBar>
                 <div className="sounds">记录声音 记录你</div>
-                
+                <div className='soundList'>
+                    {
+                        this.state.arr.map((item,idx)=>{
+                            return (
+                            <div 
+                                key={idx}
+                                style={{width:"100%"}}
+                            >
+                                <span style={{lineHeight:"5vh",fontSize:'1.5em'}}>{item.name}</span>
+                                <img src={require("../../image/la.jpg")} onClick={()=>this.delSound(item.lid,item.id)} alt="" style={{float:"right"}}/> 
+
+                                <audio id='audio'
+                                    src={this.state.voiceurl} 
+                                    controls='controls'
+                                    className="audio"
+                                >
+                                您的设备无法播放改语音
+                                </audio>
+                                <span style={{float:"right"}}>记录时间：{item.setdate}</span>
+                            </div>
+                            )
+                            
+                        })
+                    }
+                </div>
                 <Link to="/lover/lcsound"><img src={require("../../image/jia.jpg")}  className="lovesou-foot"   alt=""></img></Link>
             </div>
         )
