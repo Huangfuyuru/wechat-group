4ad9feeaf621d5c175801a4259384c23b9e5979a
Huangfuyuru <https://github.com/Huangfuyuru/JS-advanced-lessons.git>
2019-12-15 12:42:32 +0800

    loverlist

--- back-end/database/modules/listM.js
+++ back-end/database/modules/listM.js
@@ -1,22 +1,5 @@
 const pgdb = require('./connect');
 
-// 返回0代表成功  返回1代表失败
-/**
- *增加一个系统清单
- *
- * @param {Object} text 
- * @returns
- */
-async function addList(text){
-    let sql = 'insert into list(name) values ($1)';
-    let ret = await pgdb.query(sql,[text.name]);
-    if(ret.rowCount<=0){
-        return 1
-    }else{
-        return 0;
-    }
-
-}
 
 /**
  *查看list中所有的数据
@@ -33,19 +16,13 @@ async function findAll(){
     }
 }
 
-/**
- *
- *根据list id删除清单内容
- * @param {int} idC
- * @returnslist
- */
-async function dellist(id){
-    let sql = 'delete from list where id = $1';
-    let ret = await pgdb.query(sql,[id]);
+async function findAllId(){
+    let sql = 'select id from list';
+    let ret = await pgdb.query(sql);
     if(ret.rowCount<=0){
         return 1
     }else{
-        return 0
+        return ret.rows
     }
 }
 
@@ -65,28 +42,8 @@ async function findById(id){
     }
 }
 
-/**
- *根据id修改list内容
- 传入要修改的list id,以及要修改的内容
- 注意id类的都不能修改,所以text中可以没有id
- setdate类不用修改，所有text中可以没有setdate字段
- 有些内容不需要修改，但是要传入原内容  看函数中的字段
- *
- * @param {int} id
- * @param {Object} text
- * @returns
- */
-async function changeById(id,text){
-    let sql = 'update loverVoice set name=$1 where id = $2'
-    let ret = await pgdb.query(sql,[text.name,id]);
-    if(ret.rowCount<=0){
-        return 1
-    }else{
-        return 0;
-    }
-}
 
 var listM = {
-    addList,findAll,dellist,changeById,findById
+    findAll,findById,findAllId
 }
 module.exports = listM;
