b75ffd5d9095dd8e99bc0cab0472a70adb2b8a90
wuxueyao <2693088968@qq.com>
2019-12-16 14:00:56 +0800

    child

--- fore-end/demo/remember/src/container/child/child_details/Addpictures.js
+++ fore-end/demo/remember/src/container/child/child_details/Addpictures.js
@@ -31,6 +31,7 @@ export default class Addpictures extends Component {
     }
     addPictures=()=>{
         console.log(this.state.lists,this.state.pid)
+        var lists=JSON.stringify(this.state.lists)
         var addcpictureswarn = document.getElementById('addcpictureswarn');
         addcpictureswarn.style.display='block';
         if(this.state.lists.length==0){
@@ -44,7 +45,7 @@ export default class Addpictures extends Component {
                 headers:{
                     'Content-Type':"application/x-www-form-urlencoded"
                 },
-                body:`childPhotoListid=${this.state.pid}&imgurl=${this.state.lists} `
+                body:`childPhotoListid=${this.state.pid}&imgurl=${lists} `
             }).then(res=>res.json())
             .then(json=>{
                 this.setState({
