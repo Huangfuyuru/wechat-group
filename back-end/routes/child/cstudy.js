cba756921281b84717759f47ca14e0d02303a33f
Huangfuyuru <https://github.com/Huangfuyuru/JS-advanced-lessons.git>
2020-04-09 14:44:47 +0800

    a

--- back-end/routes/child/cstudy.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'),
-      app = express(),
-      router = express.Router();
-router.get('/',function(req,res,next){
-    res.end('/child/cstudy')
-})
-
-module.exports = router;
