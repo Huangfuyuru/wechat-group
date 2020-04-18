95e6e99dafbc5eeee909e3c5251f1d197b8902d8
wangshengying <1600700160@qq.com>
2020-04-18 21:55:04 +0800

    my

--- fore-end/demo/rememberapp/container/My.js
+++ fore-end/demo/rememberapp/container/My.js
@@ -1,12 +1,122 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { 
+    Text, 
+    StyleSheet, 
+    Dimensions, 
+    View, 
+    Image, 
+    TouchableOpacity, 
+    AsyncStorage,
+    StatusBar,
+    ScrollView,
+    FlatList,
+    ImageBackground,
+} from 'react-native'
+import {
+    Actions
+} from 'react-native-router-flux'
+import { Flex, WingBlank } from '@ant-design/react-native'
+const { width, scale, height } = Dimensions.get('window');
+const s1 = width / 640;
+const h = height / 1012;
 
 export default class My extends Component {
     render() {
         return (
-            <View>
-                <Text> 我的 </Text>
+            <View style={{ 
+                width: width, 
+                height: height, 
+                backgroundColor: "#fff",
+            }}>
+                <StatusBar 
+                    backgroundColor='#FFBF2D'
+                />
+                <View style={styles.navbar}>
+                    <Text style={{
+                        fontWeight: 'bold',
+                        fontSize: 20,
+                        textIndent: 3,
+                        letterSpacing: 3,
+                        color: "#ffff",
+                        lineHeight: 40
+                    }}
+                    >我的</Text>
+                </View>
+                <WingBlank style={{flex:1}}>
+                    <View style={styles.lover_second}>
+                        <Flex justify="center">
+                            <TouchableOpacity 
+                                onPress={()=>Actions.Crelation()}
+                                style={styles.btn}>
+                                <Text style={styles.blockbtn}>增加爱人</Text >
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={()=>Actions.Crelation2()} 
+                                style={styles.btn}>
+                                <Text style={styles.blockbtn}>增加亲子</Text >
+                            </TouchableOpacity>
+                            <TouchableOpacity 
+                                onPress={()=>Actions.Message()}
+                                style={styles.btn}>
+                                <Text style={styles.blockbtn}>消息反馈</Text >
+                            </TouchableOpacity>
+                        </Flex>
+                        <Flex justify="center">
+                            <TouchableOpacity 
+                                onPress={()=>Actions.Use()}
+                                style={styles.btn}>
+                                <Text style={styles.blockbtn}>设置</Text >
+                            </TouchableOpacity>
+                            <TouchableOpacity 
+                                onPress={()=>Actions.Delrelation()}
+                                style={styles.btn}>
+                                <Text style={styles.blockbtn}>删除爱人</Text >
+                            </TouchableOpacity>
+                            <TouchableOpacity 
+                                onPress={()=>Actions.Delrelation2()}
+                                style={styles.btn}>
+                                <Text style={styles.blockbtn}>删除亲子</Text >
+                            </TouchableOpacity>
+                        </Flex>
+                    </View>
+                </WingBlank>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    navbar:{
+        width:width,
+        height:65*s1,
+        backgroundColor:'#FFBF2D',
+        flexDirection: 'row',
+        paddingTop:'1%',
+        justifyContent:"center"
+    },
+    lover_second: {
+        marginBottom:15,
+        width:'100%',
+        height: 140*h,
+        flexDirection: "column",
+        justifyContent: "center",
+        marginLeft:'auto',
+        marginRight:'auto'
+    },
+    btn: {
+        padding:0,
+        height: 55*h,
+        width: "31%",
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+        backgroundColor: '#FFBF2D',
+        borderRadius: 5,
+    },
+    blockbtn:{ 
+        textAlign: "center", 
+        fontSize: 20,
+        textAlignVertical:'center', 
+        lineHeight: 55*h, 
+        color: "#fff" 
+    },
+})
--- fore-end/demo/rememberapp/container/my/Message.js
+++ fore-end/demo/rememberapp/container/my/Message.js
@@ -1,54 +1,69 @@
-import React, { Component } from 'react';
-import { NavBar, Icon } from 'antd-mobile';
-import {Link} from 'react-router-dom';
-
+import React, { Component } from 'react'
+import { 
+    Text, 
+    View,
+} from 'react-native'
 export default class Message extends Component {
-    constructor(){
-        super();
-        this.state={
-
-        }
-    }
-    render(){
-        return(
-            <div className="All">
-                <NavBar
-                style={{
-                top:0,
-                width:'100%',
-                zIndex:'11',
-                position:'fixed',
-                height:'8vh',
-                background:'#FFBF2D',
-                color:'#fff',
-                fontWeight:'bolder',
-                }}
-                mode="light"
-                icon={'𡿨'}
-                onLeftClick={() => this.props.history.push('/index/my')}
-                ><span style={{
-                    fontWeight:'bold',
-                    fontSize:'6vw',
-                    textIndent:'3vw',
-                    letterSpacing:'3vw',
-                    color:"white"
-                }}
-                >消息反馈</span>
-            </NavBar>
-            <div style={{width:"100%",height:"10px",marginTop:"15%"}}></div>
-            <div style={{marginTop:"20px"}} className="msg1">
-                <img src={require('../../image/server.png')} />
-                你好，请问有什么问题吗？
-            </div>
-            <div className="msg2">
-                不能上传头像
-                <img src={require('../../image/user.png')}/>
-            </div>
-            <div className="msg1">
-                <img src={require('../../image/server.png')} />
-                反馈已收到，尽快解决
-            </div>
-        </div>
+    render() {
+        return (
+            <View>
+               <Text>消息反馈</Text>
+            </View>
         )
     }
 }
+
+// import React, { Component } from 'react';
+// import { NavBar, Icon } from 'antd-mobile';
+// import {Link} from 'react-router-dom';
+
+// export default class Message extends Component {
+//     constructor(){
+//         super();
+//         this.state={
+
+//         }
+//     }
+//     render(){
+//         return(
+//             <div className="All">
+//                 <NavBar
+//                 style={{
+//                 top:0,
+//                 width:'100%',
+//                 zIndex:'11',
+//                 position:'fixed',
+//                 height:'8vh',
+//                 background:'#FFBF2D',
+//                 color:'#fff',
+//                 fontWeight:'bolder',
+//                 }}
+//                 mode="light"
+//                 icon={'𡿨'}
+//                 onLeftClick={() => this.props.history.push('/index/my')}
+//                 ><span style={{
+//                     fontWeight:'bold',
+//                     fontSize:'6vw',
+//                     textIndent:'3vw',
+//                     letterSpacing:'3vw',
+//                     color:"white"
+//                 }}
+//                 >消息反馈</span>
+//             </NavBar>
+//             <div style={{width:"100%",height:"10px",marginTop:"15%"}}></div>
+//             <div style={{marginTop:"20px"}} className="msg1">
+//                 <img src={require('../../image/server.png')} />
+//                 你好，请问有什么问题吗？
+//             </div>
+//             <div className="msg2">
+//                 不能上传头像
+//                 <img src={require('../../image/user.png')}/>
+//             </div>
+//             <div className="msg1">
+//                 <img src={require('../../image/server.png')} />
+//                 反馈已收到，尽快解决
+//             </div>
+//         </div>
+//         )
+//     }
+// }
